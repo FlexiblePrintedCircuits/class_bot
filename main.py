a52bd826435e405e2cd96b5a3bb589fbfae9508c
Hayata Yuge <18440@toba.kosen-ac.jp>
2019-07-02 22:00:17 +0900

    new commit

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ handler = WebhookHandler("d03905b4dadc5f6292597c595f3df85e")
 @app.route("/get_mail", methods=['POST'])
 def get_jeson():
     group_id = "C84a3f6c8f5e45507cdc2b6759bf558ac"
-    mail_body = request.form['body']
+    mail_body = request.json
 
     messages = TextSendMessage(text=mail_body)
     line_bot_api.push_message(group_id, messages=messages)
@@ -53,6 +53,46 @@ def callback():
 def handle_message(event):
     if event.type == "message":
         if (event.message.text == "Check: GroupID"):
+            line_bot_api.reply_message(
+                event.reply_token,
+                [
+                    TextSendMessage(text=event.source.group_id)
+                ]
+            )
+        if (event.message.text == "月曜日"):
+            text = "月曜日の時間割は以下の通りです。\n1-2: EC\n3-4: 微分積分\n5-6: 歴史\n7-8: 国語\n授業変更の可能性があります。必ず確認しましょう。"
+            line_bot_api.reply_message(
+                event.reply_token,
+                [
+                    TextSendMessage(text=event.source.group_id)
+                ]
+            )
+        if (event.message.text == "火曜日"):
+            text = "火曜日の時間割は以下の通りです。\n1-2: EE\n3-4: 理科\n5-6: 物理\n授業変更の可能性があります。必ず確認しましょう。"
+            line_bot_api.reply_message(
+                event.reply_token,
+                [
+                    TextSendMessage(text=event.source.group_id)
+                ]
+            )
+        if (event.message.text == "水曜日"):
+            text = "水曜日の時間割は以下の通りです。\n1-2: 微分積分\n3-4: 代数幾何\n5-6: 一般基礎教育\n7: HR\n授業変更の可能性があります。必ず確認しましょう。"
+            line_bot_api.reply_message(
+                event.reply_token,
+                [
+                    TextSendMessage(text=event.source.group_id)
+                ]
+            )
+        if (event.message.text == "木曜日"):
+            text = "木曜日の時間割は以下の通りです。\n1-2: 工学数理基礎\n3-4: 保健体育\n5-8: プログラミング\n授業変更の可能性があります。必ず確認しましょう。"
+            line_bot_api.reply_message(
+                event.reply_token,
+                [
+                    TextSendMessage(text=event.source.group_id)
+                ]
+            )
+        if (event.message.text == "金曜日"):
+            text = "金曜日の時間割は以下の通りです。\n1-4: 電気電子工学\n5-6: EC\n授業変更の可能性があります。必ず確認しましょう。"
             line_bot_api.reply_message(
                 event.reply_token,
                 [
