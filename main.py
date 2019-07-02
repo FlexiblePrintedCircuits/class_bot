f556f25e562115807b891916526019f9d62f6bdb
Hayata Yuge <18440@toba.kosen-ac.jp>
2019-07-02 21:38:07 +0900

    new commit

--- main.py
+++ main.py
@@ -17,26 +17,20 @@ app.debug = False
 line_bot_api = LineBotApi("rSGR6J4mVY3fQ8SqrCZyjtAxqT9dynuIGC87wtEGcbwLzxSGDMY2/l8YRD3cqxOcYY9JReg5uvD2kfyGGUdYp9yTWuoxgzFtyI5avM71zqwdCf4HuskTzn31LKFdAGnOsgLIt4fItpr1wOmQj5HN7wdB04t89/1O/w1cDnyilFU=")
 handler = WebhookHandler("d03905b4dadc5f6292597c595f3df85e")
 
-'''@app.route("/", methods=['GET'])
-def webhook():
-    PushDis = GetMail()
-
-    if PushDis == 10:
-        user_id = "Ue8baeea0f29de588e397c74e7b3dcf31"
-
-        messages = TextSendMessage(text="あと１０ｍ以下です！早めの補充を！")
-        line_bot_api.push_message(user_id, messages=messages)
-
-        messagesImage = ImageSendMessage(
-            original_content_url="https://cdn-ak.f.st-hatena.com/images/fotolife/h/hahayata/20190214/20190214182927.jpg",
-            preview_image_url="https://cdn-ak.f.st-hatena.com/images/fotolife/h/hahayata/20190214/20190214182927.jpg"
-        )
-        line_bot_api.push_message(user_id, messages=messagesImage)'''
-
 @app.route("/get_mail", methods=['POST'])
 def get_jeson():
+    group_id = "C84a3f6c8f5e45507cdc2b6759bf558ac"
     mail_body = request.form['body']
 
+    messages = TextSendMessage(text=mail_body)
+    line_bot_api.push_message(group_id, messages=messages)
+
+@app.route("/test", methods=['GET'])
+def test():
+    group_id = "C84a3f6c8f5e45507cdc2b6759bf558ac"
+
+    messages = TextSendMessage(text="テスト：GET通信：テスト成功")
+    line_bot_api.push_message(group_id, messages=messages)
 
 @app.route("/callback", methods=['POST'])
 def callback():
